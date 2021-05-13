0e6d785cc9e514faf1089af0e6d5c2aeca1aecb1
betulkaplan <betulkaplan7@gmail.com>
2021-05-13 21:20:40 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Card from './components/Card';
 import CW from './assets/cw.svg';
+import footerLogo from './assets/design.svg';
 
 function App() {
   return (
     <div className="App">
       <img className="cw-header" src={CW} alt="" />
       <Card></Card>
+      <div className="footer">
+        <span className="code">{'<ed8en/>'}</span>{' '}
+        <img src={footerLogo} alt="" /> <span>design</span>
+      </div>
     </div>
   );
 }
